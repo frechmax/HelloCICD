59450c617d976d600eb59ac75dacfc30778c1c79
Max Hammer <hammer.max.ulm@gmail.com>
2025-12-17 12:20:04 +0100

    first commit

---
+++ hellocicd.py
@@ -0,0 +1,19 @@
+import datetime
+
+now = datetime.datetime.now()
+html_content = f"""
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>Timestamp: {now}</p>
+</body>
+</html>
+"""
+
+with open('output/hello.html', 'w', encoding='utf-8') as f:
+    f.write(html_content)
