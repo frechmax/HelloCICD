510ffd0a117f5989cebfe63ec8bd7742012bed3b
Max Hammer <hammer.max.ulm@gmail.com>
2025-12-17 15:01:53 +0100

    output ordner weg

--- hellocicd.py
+++ hellocicd.py
@@ -7,4 +7,5 @@ m = folium.Map(location=[52.52, 13.405], zoom_start=10)
 folium.Marker([52.52, 13.405], popup="Berlin").add_to(m)
 
 # Save the map to output/index.html
-m.save('output/index.html')
+m.save('index.html')
+
